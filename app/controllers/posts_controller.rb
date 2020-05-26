da1f0419759790f80aebdca369b8b2e07124c608
camelia svennes <camelia.svennes@gmail.com>
2020-05-26 16:24:18 +0100

    as a user I can create/update a bid + footer

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,6 @@ class PostsController < ApplicationController
   end
 
   def show
-
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,7 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+  # Character.create(name: 'Luke', movie: movies.first)
+user = User.create(email: "camelia@example.com", password: "123456")
+post = Post.create(title: "Help with shopping", content: "Elderly woman needs help to buy groceries", date: Date.today, completed: false, user: User.first)
