87b3bbad1f3998a426a21828921556a85d6cec19
Zack Rose <zackrose21@gmail.com>
2020-05-28 15:51:59 +0100

    Mapbox stage 1

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,11 @@ class Post < ApplicationRecord
   has_many :bids, dependent: :destroy
   has_one :review, dependent: :destroy
   has_many :chatrooms
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+  before_validation :set_address
+  private
+  def set_address
+    self.address = self.user.address
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_125846) do
+ActiveRecord::Schema.define(version: 2020_05_28_143407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,9 @@ ActiveRecord::Schema.define(version: 2020_05_28_125846) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -84,6 +87,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_125846) do
     t.float "latitude"
     t.float "longitude"
     t.string "nickname"
+    t.string "postcode"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
