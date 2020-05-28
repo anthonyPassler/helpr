a84c34206009d251ee64a7ef522a426a6ca74b69
camelia svennes <camelia.svennes@gmail.com>
2020-05-28 10:48:41 +0100

    did the post controller, changed the footer, created the post new, once a post is completed it will be redirected to dashboard

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,26 +6,48 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @user = @post.user
-
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
-
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
-
+    @post = Post.find(params[:id])
+    if params[:completed] == "true"
+      @post.completed = true
+      @post.save
+      redirect_to dashboard_path, notice: "post marked as completed"
+    elsif @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
+
+    private
+
+    def post_params
+      params.require(:post).permit(:title, :content, :date)
+    end
 end
