4b7fac87c16625c738ed69b755066faf74f6423e
Zack Rose <zackrose21@gmail.com>
2020-05-29 14:25:33 +0100

    checklist complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :photo])
