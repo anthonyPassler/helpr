c71fa4211fc0af49903581a23156eb7e8c6f2875
Anthony Passler <passleranthony@yahoo.com>
2020-06-03 18:47:48 +0100

    added dependant destroy on chatroom model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :bids, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  has_one :chatroom
+  has_one :chatroom, dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   before_validation :set_address
